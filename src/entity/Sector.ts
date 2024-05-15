f52702704cb9d9ddfa91512fc00f987343ce7941
jhernandezkio <foreveronline_bkn@hotmail.com>
2024-05-14 21:15:28 -0400

    hah

--- src/entity/Sector.ts
+++ src/entity/Sector.ts
@@ -3,19 +3,19 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 @Entity({ name: 'SECTOR'})
 export class Sector {
 
-    @PrimaryGeneratedColumn({name: 'id'})
+    @PrimaryGeneratedColumn({ name: 'id' })
     id: number;
 
-    @Column({name: 'codigo'})
+    @Column({ name: 'codigo' })
     codigo: number;
 
-    @Column({name: 'descrip'})
+    @Column({ name: 'descrip' })
     descrip: string;
 
-    @Column({name: 'dia_car'})
+    @Column({ name: 'dia_car' })
     diaCar: number;
 
-    @Column({name: 'cod_cob'})
+    @Column({ name: 'cod_cob' })
     codCob: number;
 
 }
